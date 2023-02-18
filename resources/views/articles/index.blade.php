9babdf57168397e0813b10012c484400adf52d36
Jheng <a0960073588@gmail.com>
2023-02-18 17:07:35 +0800

    fix:把路徑/都改成 . feat(layouts/articles)：創建blade檔案

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,1 +1,9 @@
-wefwefew
+@extends('layouts.articles')
+
+@section('main')
+    <h1 class="font-thin text-4x1">文章列表</h1>
+ 
+
+  <!--  <a href="/articles/create">新增文章</a> 因為用自動創出7條路徑所以路徑必須為creeate -->
+<a href="{{ route('articles.create') }}">新增文章</a>
+@endsection
