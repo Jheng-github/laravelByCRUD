f9972ca69455036a61ba9931c2bfcbf3542b9063
Jheng <a0960073588@gmail.com>
2023-02-18 16:13:14 +0800

    feat(articles/index.blade.php) 在controller 裡面導向到index.blade.php的控制

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -7,4 +7,8 @@ use Illuminate\Http\Request;
 class ArticlesController extends Controller
 {
     //
+    public function index(){
+
+        return view('articles/index');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,29 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 use Illuminate\Http\Request;
- 
-Route::get('/test/{name?}', function ($name = null) {
-    return $name;
-});
+use App\Http\Controllers\ArticlesController;    
+Route::resource('articles', ArticlesController::class);
+Route::get('/', [ArticlesController::class, 'index'])->name('root');//文章列表當成首頁
 
 
-Route::get('/', function () {
-    
-    return view('welcome');
-});
 
+//Route::resource('articles', \app\Http\Controllers\ArticlesController::class);
+
+
+
+
+
+
+// 
 //Route::view('/test', 'test', ['name' => 'gggs']); //
 
 // Route::get('/test/{id}', function ($id) {
